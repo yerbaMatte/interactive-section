4c0e92669236f51965a5a4861fa4bc38804b703c
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2022-11-30 18:34:40 +0100

    newest version

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 const postContainer = document.querySelector('.post-container');
 let jsonData;
 
-async function addPost() {
-  let htmlCode;
+async function generatePostBox() {
+  let commentCode;
+  let replyCode;
   try {
     // the `fetch()` call will either return a Response or throw an error
     const response = await fetch('./data.json');
@@ -17,33 +18,56 @@ async function addPost() {
     console.error(`Could not get products: ${error}`);
   }
   // sort main comments from the best and display them
-
-  htmlCode = addHTML(0);
-  postContainer.insertAdjacentHTML('afterbegin', htmlCode);
-
-  console.log(jsonData.comments);
+  const orderMain = jsonData.comments.sort((x, i) => x.score - i.score);
+  orderMain.forEach((element) => {
+    console.log(element);
+    commentCode = displayPost(element, '');
+    postContainer.insertAdjacentHTML('afterbegin', commentCode);
+    // check if main comment has replies
+    if (!(element.replies.length === 0)) {
+      // sort replies
+      const orderReplies = element.replies.sort((x, i) => x.score - i.score);
+      // display replies below comment
+      orderReplies.forEach((reply) => {
+        replyCode = displayPost(reply, 'replyClass');
+        postContainer.insertAdjacentHTML('afterbegin', replyCode);
+      });
+    }
+  });
 }
 
-addPost();
+generatePostBox();
 
-function addHTML(person) {
+function displayPost(person, replyClass) {
   return `
-  <div class="post-box">
+  <div class="post-box ${replyClass}">
   <div class="likes-box">
   <button class="btn-rate"><img src="./images/icon-plus.svg" alt="plus"></img></button>
-    <p class="likes-counter">${jsonData.comments[person].score}</p>
+    <p class="likes-counter">${person.score}</p>
     <button class="btn-rate"><img src="./images/icon-minus.svg" alt="minus"></button>
   </div>
   <div class="post-content">
     <div class="post-header">
       <div class="post-info">
-        <img src="${jsonData.comments[person].user.image.webp}" alt="profile-pic" class="profile-pic" />
-        <span class="profile-name">${jsonData.comments[person].user.username}</span>
-        <p class="time">${jsonData.comments[person].createdAt}</p>
+        <img src="${person.user.image.webp}" alt="profile-pic" class="profile-pic" />
+        <span class="profile-name">${person.user.username}</span>
+        <p class="time">${person.createdAt}</p>
       </div>
       <button class="reply">REPLY</button>
     </div>
-    <div class="post-text">${jsonData.comments[person].content}</div>
+    <div class="post-text">${person.content}</div>
   </div>
-</div>`;
+</div> `;
 }
+
+// let dogs;
+
+// const doggosy = async function () {
+//   const res = await fetch('https://dog.ceo/api/breeds/list/all');
+//   console.log(res);
+//   const data = await res.json();
+//   console.log(data);
+//   dogs = data;
+// };
+
+// doggosy();
